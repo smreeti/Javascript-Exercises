eb474c960632b89d4c5b8165b448d5c41e8dcf14
smriti.mool <mool.smreeti@gmail.com>
2022-06-02 18:10:24 -0400

    add temperature converter

--- JS_objects_functions_events-02/convert_temps/convert_temp.js
+++ JS_objects_functions_events-02/convert_temps/convert_temp.js
@@ -1,15 +1,48 @@
 "use strict";
-var $ = function(id) { return document.getElementById(id); };
+var $ = function (id) { return document.getElementById(id); };
 
-
-var clearTextBoxes = function() {
+var clearTextBoxes = function () {
     $("degrees_entered").value = "";
     $("degrees_computed").value = "";
 };
 
-window.onload = function() {
+window.onload = function () {
     $("convert").onclick = convertTemp;
     $("to_celsius").onclick = toCelsius;
     $("to_fahrenheit").onclick = toFahrenheit;
-	$("degrees_entered").focus();
+    $("degrees_entered").focus();
 };
+
+const toFahrenheit = () => {
+    clearTextBoxes();
+    document.getElementById("degree_label_1").innerHTML = "Enter C Degrees";
+    document.getElementById("degree_label_2").innerHTML = "Degrees Fahrenheit:";
+}
+
+const toCelsius = () => {
+    clearTextBoxes();
+    document.getElementById("degree_label_1").innerHTML = "Enter F Degrees";;
+    document.getElementById("degree_label_2").innerHTML = "Degrees Celsius:";;
+}
+
+const convertTemp = () => {
+    const degreesEntered = parseFloat($("degrees_entered").value);
+    if (isNaN(degreesEntered) || degreesEntered < 0) {
+        alert("Please enter the valid positive number");
+        clearTextBoxes();
+    } else {
+        let toCelsius = document.getElementById('to_celsius').checked;
+        let degreeComputed = toCelsius ? convertFahrenheitToCelsuis(degreesEntered) : convertCelsiusToFahrenheit(degreesEntered);
+        $("degrees_computed").value = parseFloat(degreeComputed).toFixed(0);
+    }
+}
+
+const convertFahrenheitToCelsuis = (fahrenheit) => {
+    return ((fahrenheit - 32) * 5 / 9);
+}
+
+const convertCelsiusToFahrenheit = (celsius) => {
+    return ((celsius * 9 / 5) + 32);
+}
+
+
